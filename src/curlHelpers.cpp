4f052c4ea2e8285471a79cc980634314136c94bd
Sam D <samueljohnsonday@gmail.com>
2024-11-01 23:51:54 -0500

    Add comment to WriteCallback function

--- src/curlHelpers.cpp
+++ src/curlHelpers.cpp
@@ -1,5 +1,7 @@
 #include <curlHelpers.hpp>
 
+/* Callback function used by libcurl to append
+received data into a user-defined buffer */
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
