f0fe740c6415bc98f642b2df4bdd5eeb8fe2e0e0
Sam D <samueljohnsonday@gmail.com>
2024-11-07 12:26:22 -0600

    Create and implement basic curl structure and JSON parsing for the pokeDataFetcher struct

--- include/pokeDataFetcher.hpp
+++ include/pokeDataFetcher.hpp
@@ -1,8 +1,15 @@
 #ifndef POKEMONDATAFETCHER_HPP
 #define POKEMONDATAFETCHER_HPP
 
+#include <string>
 #include <curl/curl.h>
+#include <nlohmann/json.hpp>
+#include "curlHelpers.hpp"
+#include "poke.hpp"
 
-class pokeDataFetcher {};
+struct pokeDataFetcher {
+    bool fetchData(Poke& poke);
+    void connectData(Poke& pokeToConnect, std::string readBuffer);
+};
 
 #endif
