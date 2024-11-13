5c8be653772e4472302ce1667be41e430112c699
SJD23Dev <SJD23Dev@proton.me>
2024-11-13 10:33:48 -0600

    Create skeleton code for Move class

---
+++ include/move.hpp
@@ -0,0 +1,12 @@
+#ifndef MOVES_HPP
+#define MOVES_HPP
+
+class Move {
+    public:
+        // Constructors
+        Move();
+        ~Move();
+    private:
+};
+
+#endif
---
+++ src/move.cpp
@@ -0,0 +1,9 @@
+#include "move.hpp"
+
+/* Default constructor for Move object,
+initilizes all values to default state */ 
+Move::Move() {}
+
+/* Default destructor for Move object,
+initilizes all values to default state */
+Move::~Move() {}
