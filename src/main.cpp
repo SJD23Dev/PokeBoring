ab878e6fe9d5515dece8c9bac5684e2d8f71ee43
SJD23Dev <SJD23Dev@proton.me>
2024-11-13 12:54:30 -0600

    Updated main.cpp to remove outdated testing methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,3 @@
-#include <iostream>
-#include "poke.hpp"
-
 int main() {
-    Poke poke(685);
-    pokeDataFetcher fetch;
-    fetch.fetchData(poke);
-    std::cout << poke.getName() << std::endl;
+    // Nothing here for now!
 }
