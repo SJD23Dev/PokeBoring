5b3c0e585d3a9dc36a3f564b04053074f17a8515
SJD23Dev <samueljohnsonday@gmail.com>
2024-11-15 11:23:51 -0600

    Create basic SDL2 variables window and renderer, also implement starter code for the Window class constructors

--- include/window.hpp
+++ include/window.hpp
@@ -1,11 +1,17 @@
 #ifndef WINDOW_HPP
 #define WINDOW_HPP
 
+#include <string>
+#include <stdexcept>
+#include <SDL2/SDL.h>
+
 class Window {
     public:
         Window();
         ~Window();
     private:
+        SDL_Window* window;
+        SDL_Renderer* renderer;
 };
 
 #endif
--- src/window.cpp
+++ src/window.cpp
@@ -1,5 +1,45 @@
 #include "window.hpp"
 
-Window::Window() {}
+/* Constructor for Window class, 
+creates an SDL2 window */
+Window::Window() {
+    // Initilize SDL pointers
+    window = nullptr;
+    renderer = nullptr;
 
-Window::~Window() {}
+    SDL_Init(SDL_INIT_EVERYTHING); // SDL function to initilize everything
+    
+    // Create the window!
+    window = SDL_CreateWindow("PokeBoring", 
+                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              700,
+                              750,
+                              SDL_WINDOW_SHOWN);
+
+    if (!window) { // If the window failed to start...
+        throw std::runtime_error("[Window::Window()] " + std::to_string(__LINE__) + " " + std::string(SDL_GetError()));
+    }
+
+    // Create the renderer!
+    renderer = SDL_CreateRenderer(window, 
+                                  -1, 
+                                  SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if (!renderer) { // If the renderer failed to start...
+        SDL_DestroyWindow(window);
+        throw std::runtime_error("[Window::Window()] " + std::to_string(__LINE__) + " " + std::string(SDL_GetError()));
+    }                            
+}
+
+/* Destructor for Window class */
+Window::~Window() {
+    if (renderer) { // If the renderer is open, destroy it
+        SDL_DestroyRenderer(renderer);
+    }
+
+    if (window) { // If the window is open, destroy it
+        SDL_DestroyWindow(window);
+    }
+
+    SDL_Quit(); // Close down SDL (opposite function to SDL_Init())
+}
